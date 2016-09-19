9780316e1f701cffa6b87250f415116bc94edb56
pooja kamat <pooja kamat>
2016-09-19 15:40:41 -0400

    Selection Sort

--- com.practise/src/main/java/com/practise/SelectionSortStrategy.java
+++ com.practise/src/main/java/com/practise/SelectionSortStrategy.java
@@ -1,5 +1,51 @@
 package com.practise;
 
-public class SelectionSortStrategy {
+/**
+ * Created by pkama on 9/19/2016.
+ */
+public class SelectionSortStrategy implements SortingStrategy {
+
+    private static SelectionSortStrategy _instance;
+
+    private SelectionSortStrategy(){
+
+    }
+
+    public static SelectionSortStrategy getInstance(){
+        if(_instance == null){
+            _instance = new SelectionSortStrategy();
+        }
+        return _instance;
+    }
+
+    public void sort(int[] arr) {
+        if(arr == null){
+            throw new IllegalArgumentException("Array is null");
+        }
+        else {
+
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                int index = i;
+                for (int j = i + 1; j < arr.length; j++)
+                    if (arr[j] < arr[index])
+                        index = j;
+
+                int smallerNumber = arr[index];
+                arr[index] = arr[i];
+                arr[i] = smallerNumber;
+                printNumbers(arr);
+            }
+        }
+    }
+
+    private void printNumbers(int[] input) {
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + ", ");
+        }
+        System.out.println("\n");
+    }
+
 
 }
--- com.practise/src/test/java/com/practise/test/SortingUtilTest.java
+++ com.practise/src/test/java/com/practise/test/SortingUtilTest.java
@@ -1,6 +1,7 @@
 package com.practise.test;
 
 import com.practise.InsertionSortStrategy;
+import com.practise.SelectionSortStrategy;
 import com.practise.SortingUtil;
 import com.practise.Util;
 import org.junit.Assert;
@@ -37,6 +38,35 @@ public class SortingUtilTest {
         int[] array1={2, 1, 0 , 2, 8, 7, 2};
         int[] array2={0, 1, 2, 2, 2, 7, 8};
         SortingUtil sortingUtil = new SortingUtil(InsertionSortStrategy.getInstance());
+        sortingUtil.sort(array1);
+        Assert.assertTrue(Arrays.equals(array1,array2));
+    }
+
+    @Test
+    public void selectionSortNullTest(){
+        Exception ex =null;
+        try{
+            SortingUtil sortingUtil = new SortingUtil(SelectionSortStrategy.getInstance());
+            sortingUtil.sort(null);
+        }
+        catch (Exception e){
+            ex=e;
+        }
+        Assert.assertTrue(ex!=null && ex instanceof IllegalArgumentException && ex.getMessage().equals("Array is null"));
+    }
+
+    @Test
+    public void selectionSortTest(){
+        int[] array1={1, 9, 0, 4, 12, 34, 6, 23, 2};
+        int[] array2={0, 1, 2, 4, 6, 9, 12, 23, 34};
+        SortingUtil sortingUtil = new SortingUtil(SelectionSortStrategy.getInstance());
+    }
+
+    @Test
+    public void selectionSortDuplicatedValueTest(){
+        int[] array1={2, 1, 0 , 2, 8, 7, 2};
+        int[] array2={0, 1, 2, 2, 2, 7, 8};
+        SortingUtil sortingUtil = new SortingUtil(SelectionSortStrategy.getInstance());
         sortingUtil.sort(array1);
         Assert.assertTrue(Arrays.equals(array1,array2));
     }
